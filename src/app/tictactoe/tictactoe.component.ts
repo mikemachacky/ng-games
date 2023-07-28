4aad41420340cee034f74269c96726115b69de7d
magda_m <magdamachacka@gmail.com>
2023-07-28 13:42:26 +0200

    logic works
    primitive but works

--- src/app/tictactoe/tictactoe.component.ts
+++ src/app/tictactoe/tictactoe.component.ts
@@ -6,34 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./tictactoe.component.css']
 })
 export class TictactoeComponent {
-  tiles:string[][] = [
-    ['','',''],
-    ['','',''],
-    ['','',''],
+  tiles: string[][] = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
   ];
-  whoPlaysNow:string = "O";
 
-  currentPlayer(){
-    if(this.whoPlaysNow=="O")
-    return "X";
-    else
-    return"O";
+  whoPlaysNow: string = 'O';
+  movesCounter: number = 0;
+  winner: string = '';
+  isDraw: boolean = false;
+
+  currentPlayer(): string {
+    return this.whoPlaysNow === 'O' ? 'X' : 'O';
   }
 
-  newGamePressed():void{
-    this.tiles =  [
-      ['','',''],
-      ['','',''],
-      ['','',''],
+  newGamePressed(): void {
+    this.tiles = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
     ];
-  this.whoPlaysNow = "O";
+    this.whoPlaysNow = 'O';
+    this.movesCounter = 0;
+    this.winner = '';
+    this.isDraw = false;
   }
 
-  cellClikced(rowIndex:number,colIndex:number):void{
-    if(this.tiles[rowIndex][colIndex]==''){
-      this.tiles[rowIndex][colIndex]=this.whoPlaysNow;
-      this.whoPlaysNow = this.currentPlayer();
+  cellClicked(rowIndex: number, colIndex: number): void {
+    if (this.tiles[rowIndex][colIndex] === '' && !this.winner) {
+      this.tiles[rowIndex][colIndex] = this.whoPlaysNow;
+      this.movesCounter++;
+      if (this.checkGameStatus(rowIndex, colIndex)) {
+        this.winner = this.whoPlaysNow;
+      } else if (this.movesCounter === 9) {
+        this.isDraw = true;
+      } else {
+        this.whoPlaysNow = this.currentPlayer();
+      }
     }
+  }
 
+  checkGameStatus(rowIndex: number, colIndex: number): boolean {
+    const player = this.tiles[rowIndex][colIndex];
+    if (
+      this.tiles[rowIndex][0] === player &&
+      this.tiles[rowIndex][1] === player &&
+      this.tiles[rowIndex][2] === player
+    ) {
+      return true;
+    }
+    if (
+      this.tiles[0][colIndex] === player &&
+      this.tiles[1][colIndex] === player &&
+      this.tiles[2][colIndex] === player
+    ) {
+      return true;
+    }
+    if (
+      (this.tiles[0][0] === player &&
+        this.tiles[1][1] === player &&
+        this.tiles[2][2] === player) ||
+      (this.tiles[0][2] === player &&
+        this.tiles[1][1] === player &&
+        this.tiles[2][0] === player)
+    ) {
+      return true;
+    }
+    return false;
   }
 }
